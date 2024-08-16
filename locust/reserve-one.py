8a2845d7c56e2e1c79c9835c79acac88c7057f13
JunseokOh <ixtears23@icloud.com>
2024-08-17 02:44:58 +0900

    feature 예약 부하 테스트를 위한 locust파일 추가

---
+++ locust/reserve-one.py
@@ -0,0 +1,12 @@
+import random
+from locust import HttpUser, task, between
+
+class ReservationTest(HttpUser):
+    network_timeout = 30.0
+    connection_timeout = 30.0
+
+    @task
+    def reserve_class(self):
+        user_id = random.randint(1, 1000000)
+        class_id = 1
+        self.client.post(f"/v1/classes/{class_id}/reserve?userId={user_id}")
